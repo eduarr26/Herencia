4986a3d6f059c93e78da79e5d78b3a8e6edbb9a2
Andres Eduardo Hiraldo <20221692@itla.edu.do>
2024-02-02 22:44:19 -0400

    .

---
+++ Herencia/Administrativo.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Herencia
+{
+    public class Admininstrativo : Empleado
+    {
+        public string? Departamento { set; get; }
+        public string? DescripcionPuesto { set; get; }
+
+        public void Copias()
+        {
+            Console.WriteLine("Sacando copias");
+        }
+    }
+}
---
+++ Herencia/Maestro.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Herencia
+{
+    public class Maestro : Docente
+    {
+        public string? materia { set; get; }
+        public string? calendario { set; get; }
+
+        public void Lista()
+        {
+            Console.WriteLine("Pasar Lista");
+        }
+    }
+}
